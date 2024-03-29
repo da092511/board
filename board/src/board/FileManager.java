8a63c5b112c301184e3d2998a34f8dbe7c065796
dayeon <da092511@naver.com>
2024-03-29 18:31:52 +0900

    Add : mehtod(loadFile,saveData)

--- board/src/board/FileManager.java
+++ board/src/board/FileManager.java
@@ -1,5 +1,45 @@
 package board;
 
-public class FileManager {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class FileManager {
+	private String fileName = "Cbs.txt";
+	private File file = new File(fileName);
+	
+	private FileWriter fw;
+	private FileReader fr ;
+	private BufferedReader br;
+	
+	public void saveFile(String data) {
+		
+	}
+	
+	public String loadFile() {
+		String info = "";
+		if(file.exists()) {
+			
+			try {
+				fr = new FileReader(file);
+				br = new BufferedReader(fr);
+				
+				
+				fr.close();
+				br.close();
+				System.out.println("load complete");
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("load failed");
+			}
+			
+		}else 
+			return null;
+		
+		
+		return info;
+	}
+	
 }
