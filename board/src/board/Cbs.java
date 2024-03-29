2cb803a35ec83edf0d642c93cec56265ecbafb58
dayeon <da092511@naver.com>
2024-03-29 15:25:25 +0900

    Add : method(showContent, content, openContent...)

--- board/src/board/Cbs.java
+++ board/src/board/Cbs.java
@@ -10,20 +10,26 @@ public class Cbs {
 	private Scanner scanner = new Scanner(System.in);
 	private Random random = new Random();
 
+	private final int PAGE = 5;
+	
 	private final int JOIN = 1;
 	private final int SET_LOG = 2;
 	private final int ALL_CONTENT = 3;
-	private final int MY_PAGE = 4;
+	private final int WRITE_CONTENT = 4;
+	private final int MY_PAGE = 5;
 	
 	private ArrayList<User> users;
-	private Map <User, ArrayList<Board>> boards;
+	private ArrayList<Board> boards;
+	
+	private Map <User, ArrayList<Board>> owners;
 	
 	
 	private int log = -1;
 	
 	private Cbs() {
 		users = new ArrayList<>();
-		boards = new HashMap<User, ArrayList<Board>>();
+		boards = new ArrayList<>();
+		owners = new HashMap<User, ArrayList<Board>>();
 	}
 	
 	private static Cbs instance = new Cbs();
@@ -36,7 +42,8 @@ public class Cbs {
 		System.out.println("1) 회원가입");
 		System.out.println(String.format("2)%s", log == -1 ? "로그인" : "로그아웃"));
 		System.out.println("3)전체 게시물");
-		System.out.println("4)마이페이지");
+		System.out.println("4)게시물 작성");
+		System.out.println("5)마이페이지");
 	}
 	
 	private int checkUserByCode(int code) {
@@ -132,6 +139,91 @@ public class Cbs {
 		System.out.println("로그인 성공");
 	}
 	
+	private void showContent(int firstIndex) {
+		int lastIndex = firstIndex + PAGE;
+		
+		if(lastIndex >= boards.size())
+			lastIndex = boards.size();
+		
+		for(int i= firstIndex;i<=lastIndex;i++) {
+			Board board = boards.get(i);
+			String title = board.getTitle();
+			
+			System.out.println(title);
+		}
+		
+	}
+	
+	private void showContentMenu() {
+		System.out.println("1)이전 페이지");
+		System.out.println("2)다음 페이지");
+		System.out.println("3)게시물 선택");
+		System.out.println("0)뒤로가기");
+	}
+	
+	private void runContentMenu(int option) {
+		
+	}
+	
+	private void openContent() {
+		User user = users.get(log);
+		
+		
+	}
+	
+	private void content() {
+		int firstIndex = 0;
+		
+		
+		while(true) {
+			showContent(firstIndex);
+			showContentMenu();
+			int option = inputNumber("");
+			
+			if(option == 0)
+				break;
+			
+		//	runContentMenu(option);
+			
+		}
+	}
+	
+	
+	private String writeContent() {
+		String info = "";
+		
+		while(true) {
+			String line = inputString("줄바꿈[;] 글쓰기 완료[1] 뒤로가기[0]");
+			
+			if(line.equals("0")) 
+				break;
+			else if (line.equals("1"))
+				break;
+			
+			info += line;
+		}
+		
+		return info;
+	}
+	
+	private void write() {
+		User user = users.get(log);
+		
+		boolean isCheck = false;
+		if(boards.size() > 3) {
+			isCheck = true;
+		}
+		
+		if(isCheck && !user.accessible()) {
+			System.err.println("아직 접근이 불가능합니다.");
+			return;
+		}
+		
+		String id = user.getId();
+		String title = inputString("게시글 제목");
+		String content = writeContent();
+	}
+	
 	private void runMenu(int option) {
 		switch (option) {
 		case JOIN:
@@ -141,6 +233,10 @@ public class Cbs {
 			setLog();
 			break;
 		case ALL_CONTENT:
+			content();
+			break;
+		case WRITE_CONTENT:
+			write();
 			break;
 		case MY_PAGE:
 			break;
@@ -165,7 +261,7 @@ public class Cbs {
 		message = String.format("%s : ", message);
 		System.out.println(message);
 		
-		return scanner.next();
+		return scanner.nextLine();
 	}
 	
 	private int inputNumber(String message) {
