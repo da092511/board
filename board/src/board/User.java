4f820d1be7621632017778aa263726329925778e
dayeon <da092511@naver.com>
2024-03-29 11:47:31 +0900

    Add : getId method

--- board/src/board/User.java
+++ board/src/board/User.java
@@ -1,5 +1,21 @@
 package board;
 
 public class User {
+	private int code;
+	
 	private String name ;
+	private String id;
+	private String pw;
+	
+	public User(int code, String name, String id, String pw) {
+		this.code =code;
+		this.name =name;
+		this.id = id;
+		this.pw = pw;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+
 }
