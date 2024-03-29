f7df5332dbdd8f263b0c668b623fabe97aab0b02
dayeon <da092511@naver.com>
2024-03-29 11:34:57 +0900

    Add : inputString method

--- board/src/board/Cbs.java
+++ board/src/board/Cbs.java
@@ -37,6 +37,13 @@ public class Cbs {
 		}
 	}
 	
+	private String inputString(String message) {
+		message = String.format("%s : ", message);
+		System.out.println(message);
+		
+		return scanner.next();
+	}
+	
 	private int inputNumber(String message) {
 		int number = -1;
 		message = String.format("%s : ", message);
