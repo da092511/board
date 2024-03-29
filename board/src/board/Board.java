d33e3a68b48f160eec65ee9f97d771a58b2ad3c3
dayeon <da092511@naver.com>
2024-03-29 18:18:11 +0900

    Modify : toString method(override)

--- board/src/board/Board.java
+++ board/src/board/Board.java
@@ -27,7 +27,7 @@ public class Board {
 		String info = title;
 		info += "\n----------------";
 		info += String.format("\n%s\t\t%s", this.date, this.id);
-		
+		info += "\n----------------";
 		String[] content = contents.split(";");
 		for(String line : content)
 			info += "\n" + line;
